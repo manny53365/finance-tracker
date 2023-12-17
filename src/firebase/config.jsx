c88d0c6d0764492b8a4366024b2ba0427320b8dc
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-12-17 17:11:26 +0530

    added firebase app and auth

---
+++ src/firebase/config.jsx
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD1vhXMK_yXLiFZNQMFaeL_rgXYej-kZ3s",
+    authDomain: "finance-app-d4541.firebaseapp.com",
+    projectId: "finance-app-d4541",
+    storageBucket: "finance-app-d4541.appspot.com",
+    messagingSenderId: "365800216798",
+    appId: "1:365800216798:web:29b138bcd56405d7f98a4f",
+    measurementId: "G-FH3TFNE8P0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+export { projectFirestore, projectAuth }
