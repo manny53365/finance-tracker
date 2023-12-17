00d9d85f534163c72e1744b7a4fa0118ef21174d
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-12-17 15:42:47 +0530

    added some styling and the form to the sign up page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react'
 import styles from './Login.module.css'
 
 export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(email, password)
+    }
+
     return (
-        <div>
-            Log in
-        </div>
+        <form onSubmit={handleSubmit} className={styles['login-form']}>
+            <h2>Log in</h2>
+            <label><span>Email:</span></label>
+            <input
+            type="email" 
+            onChange={(event) => setEmail(event.target.value)}
+            value={email}
+            />
+            <label><span>Password:</span></label>
+            <input
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}
+            value={password}
+            />
+            <button className='btn'>Login</button>
+        </form>
     )
 }
