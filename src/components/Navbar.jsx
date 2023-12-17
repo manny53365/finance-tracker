2e2b41c1793b43d6eafe12eddaa81e401c3852b9
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-12-17 18:41:11 +0530

    added logout button and exported the outout hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 import styles from './Navbar.module.css'
 
 export default function Navbar() {
+
+    const { logout } = useLogout();
+
     return(
         <nav className={styles.navbar}>
             <ul>
@@ -10,6 +14,7 @@ export default function Navbar() {
 
                 <li><Link to='/login'>Login</Link></li>
                 <li><Link to='/signup'>Sign up</Link></li>
+                <li><button className='btn' onClick={logout}>Logout</button></li>
             </ul>
         </nav>
     )
