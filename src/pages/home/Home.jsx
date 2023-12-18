b92b80cff6857865aba3f020ad04a6fc36e304af
Manny Rodriguez <mannyrodriguez011@gmail.com>
2023-12-18 14:21:58 +0530

    added some styling to the home screen for setup

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,13 @@ import styles from './Home.module.css'
 
 export default function Home() {
     return (
-        <div>
-            Home
+        <div className={styles.container}>
+            <div className={styles.content}>
+                Transaction list
+            </div>
+            <div className={styles.sidebar}>
+                Transaction form
+            </div>
         </div>
     )
 }
